a936019e7c71e92beec529c085ea0564a42c0f17
nelmar0729 <nelmarraymundo1@gmail.com>
2025-08-22 11:37:52 +0800

    Update Flutter Web build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8a4e3debd46a7c2404ba0f4ef76c830c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "2023898437dba58c740ad56cdb8a5d41",
+"flutter_bootstrap.js": "bd67536e150dadb28e7c017019b99eba",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
