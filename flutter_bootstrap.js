63fd58bf01136561adcb21b9cd0917dae4949e3a
nelmar0729 <nelmarraymundo1@gmail.com>
2025-09-04 01:19:38 +0800

    deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2566163219"
+    serviceWorkerVersion: "3819149735"
   }
 });
